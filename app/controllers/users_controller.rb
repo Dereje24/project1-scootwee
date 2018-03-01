b7d51aa5e616e2f9adf809fef03651ba506e1eb4
t-tullis <t.tullis34@gmail.com>
2018-03-01 09:47:26 -0800

    added signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,6 +11,8 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to user_path
   end
 
   def edit
@@ -14,4 +20,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+  def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password)
+end
+
 end
