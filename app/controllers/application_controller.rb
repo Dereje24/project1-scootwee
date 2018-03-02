bb307ee6c5c8d264f7696258b0f463cbb792397e
Abdelhalim <golden-boy1994@hotmail.fr>
2018-03-02 13:56:25 -0500

    map with pointer and direction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !current.nil?
   end
-  def login(user)
+  def login(user,lat,lng)
     session[:user_id]=user.id
+    session[:lat]=lat
+    session[:lng]=lng
+    cookies[:lat]=session[:lat]
+    cookies[:lng]=session[:lng]
   end
   def current
+    cookies[:lat]=session[:lat]
+    cookies[:lng]=session[:lng]
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Local.delete_all
+position=[37.78317009,-122.38788812,37.77009658,-122.43696013,
+37.78430452,-122.43240438,
+37.79272767,-122.42420559,
+37.78903522,-122.40411756,
+37.76153446,-122.41936672,
+37.79968137,-122.42678937,
+37.75454011,-122.44374842,
+37.75909595,-122.41999152,
+37.76408153,-122.4464512,
+]
+10.times do |index|
+Local.create(name: Faker::Name.name ,max: 10, street: Faker::Address.street_address, city: 'San francisco', state:'CA' , zip_code:'94016',lat: position[index],lng:position[index+1])
+end
