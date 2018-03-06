47bb51f64e2e5429a8c2463916c2220d1c9448e5
Dereje24 <ddilnesaw@yahoo.com>
2018-03-05 19:25:05 -0800

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root => 'home#index'
+  root to: 'home#index'
 
   get '/home', to: 'home#index'
   get '/contact_us', to: 'home#contact', as: 'contact'
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
   post '/rent', to: 'rentals#create' , as:'rental'
   get 'user/reservation', to: 'rentals#current', as:'current'
   post '/drop' , to:'rentals#drop' , as: 'drop'
-  get '/user/history', to:'rentals#history',as:'history'
-  get '/history', to::'rentals#show'
+  get '/user/history', to:'rentals#history', as:'history'
+  get '/history', to:'rentals#show'
   # get '/user/map', to: 'users#map'
 end
 
